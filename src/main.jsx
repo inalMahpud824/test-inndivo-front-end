d041a271a08c76f03a410f51c6248d1973751658
Inal Mahpud <inalmahpud1@gmail.com>
2024-03-15 22:43:48 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import ListPokemonPage from './pages/ListPokemon'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ListPokemonPage />
+  </React.StrictMode>,
+)
