776d32efd93ac462ec134a7d8cc2f46e5f4d8317
Inal Mahpud <inalmahpud1@gmail.com>
2024-03-16 00:45:19 +0700

    add and use React-Router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import MyPokemonPage from './pages/MyPokemon'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ListPokemonPage from './pages/ListPokemon'
+import DetailPage from './pages/DetailPokemon'
 
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <ListPokemonPage />,
+    
+  },
+  {
+    path: '/my-pokemon',
+    element : <MyPokemonPage />
+  }, 
+  {
+    path: '/detail-pokemon',
+    element: <DetailPage />
+  },
+
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ListPokemonPage />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
