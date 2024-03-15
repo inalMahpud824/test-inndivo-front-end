8f63e63d6dc91af5afd9b9c92de8249a2dd3e2cb
Inal Mahpud <inalmahpud1@gmail.com>
2024-03-16 02:40:17 +0700

    consume api getAllPokemon

--- src/pages/ListPokemon.jsx
+++ src/pages/ListPokemon.jsx
@@ -1,15 +1,36 @@
+import { useEffect, useState } from "react";
 import CardPokemon from "../components/CardPokemon";
 import Navbar from "../components/Navbar";
 import SearchBox from "../components/SearchBox";
+import axios from "axios";
 
 const ListPokemonPage = () => {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+const getPokemon = async () => {
+  try{
+    const response = await axios.get("http://localhost:5000/pokemon")
+    const res = await response.data
+    setPokemon(res)
+  }catch (error) {
+    console.error(error)
+  }
+}
+// pokemon.map((pkmn) => {
+//   console.log(pkmn.id)
+// })
   return (
     <>
     <Navbar />
     <SearchBox />
 
       <div className="flex gap-4 justify-start">
-        <CardPokemon title='Mudkip' />
+        {pokemon && pokemon.map((pkmn) => (
+          <CardPokemon key={pkmn.id} title={pkmn.name} id={pkmn.id} />
+        ))}
       </div>
     </>
   );
